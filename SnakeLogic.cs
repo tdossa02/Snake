aeb2bd8524b2dc20bfeff3282804b74d1b19e406
tdossa02 <32648730+tdossa02@users.noreply.github.com>
2019-02-03 15:09:08 +0000

    Update SnakeLogic.cs

--- SnakeLogic.cs
+++ SnakeLogic.cs
@@ -1,5 +1,3 @@
-﻿using System.Collections.Generic;
-
 namespace Snake
 {
 	public enum Direction
@@ -10,21 +8,8 @@ namespace Snake
 		Down
 	}
 
-	public struct Point
-	{
-		public Point(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
-
-		public int X { get; set; }
-		public int Y { get; set; }
-	}
-
 	public class SnakeLogic
 	{
-		private readonly List<Point> _snake = new List<Point>();
 
 		public bool Compute(int[][] snakeGrid, Direction snakeDirection)
 		{
@@ -35,78 +20,21 @@ namespace Snake
 			const int appleValue = 2;
 			///////////////////////////////////////////////////////////////////////////////
 
-
-
-			//lets put an apple on the grid for no reason :)
-			snakeGrid[5][5] = appleValue;
-
-
-			//erase current snake from the grid
-			for (int i = 0; i < _snake.Count; i++)
-			{
-				int x = _snake[i].X;
-				int y = _snake[i].Y;
-
-
-				//TAKE NOTE HOW y AND x are used in snakeGrid. I.e, [y][x] and not [x][y]
-				snakeGrid[y][x] = emptyValue;
-			}
-
-			//move snake according to direction
-			if (snakeDirection == Direction.Right)
-			{
-				for (int i = 0; i < _snake.Count; i++)
-				{
-					//x and y are set to the next square to the right
-					int x = _snake[i].X + 1;
-					int y = _snake[i].Y;
-					
-					//if the snake reaches the horizontal corner of the grid
-					//wrap around it
-					if (x >= snakeGrid.Length)
-					{
-						x = 0;
-					}
-					//......................................................
-					
-					_snake[i] = new Point()
-					{
-						X = x,
-						Y = y
-					};
-				}
-			}
-
-			//paint snake back onto grid
-			for (int i = 0; i < _snake.Count; i++)
-			{
-				int x = _snake[i].X;
-				int y = _snake[i].Y;
-				snakeGrid[y][x] = snakeValue;
-			}
-
-
 			//return true if you want the game to continue
 			//return false if the user died
 			return true;
 		}
-
-
 		
 		public int GetScore()
 		{
 			//the method is called to get the score
-			return 5;
+			return 0;
 		}
 
 		public void Reset()
 		{
 			//this method will be called
 			//when a new game starts
-			_snake.Clear();
-			_snake.Add(new Point(2, 0));
-			_snake.Add(new Point(1, 0));
-			_snake.Add(new Point(0, 0));
 		}
 	}
 }
